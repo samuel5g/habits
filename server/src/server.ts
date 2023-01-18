04b8ba2cc32a2451ff7ebc26d2facf3bbab67eab
samuel5g <samuelmonteiro21@gmail.com>
2023-01-17 21:30:09 -0300

    feat: :rocket: Begin project!

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+/**
+ * Método HTTP: Get, Post, Put, Patch, Delete
+ */
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany({})
+
+  return habits
+})
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('Listen on port: 3333');
+
+})

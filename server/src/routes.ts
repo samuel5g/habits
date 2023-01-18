8a149fcbdb53c9c88ba0f868b261466e8b0b5c72
samuel5g <samuelmonteiro21@gmail.com>
2023-01-18 12:33:49 -0300

    feat(routes): :sparkles: Adds create habits route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,34 @@
 import { prisma } from "./lib/prisma"
+import dayjs from 'dayjs'
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const habits = await prisma.habit.findMany({})
+  app.post('/habits', async (req, res) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+        z.number().min(0).max(6)
+      )
+    })
+    const { title, weekDays } = createHabitBody.parse(req.body)
 
-    return habits
+    const today = dayjs().startOf('day').toDate()
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map(weekDay => {
+            return {
+              week_day: weekDay
+            }
+          })
+        }
+      }
+    })
+
   })
 
 }

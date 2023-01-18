855d6c0fa16a4cb6386b2feab76161b966f698ea
samuel5g <samuelmonteiro21@gmail.com>
2023-01-18 13:03:39 -0300

    feat(routes): :sparkles: Adds route to get all habits and done habits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,7 +4,7 @@ import { FastifyInstance } from "fastify"
 import { z } from "zod"
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/habits', async (req, res) => {
+  app.post('/habits', async (req) => {
     const createHabitBody = z.object({
       title: z.string(),
       weekDays: z.array(
@@ -31,4 +31,48 @@ export async function appRoutes(app: FastifyInstance) {
 
   })
 
+  app.get('/day', async (req) => {
+    const getDayParams = z.object({
+      date: z.coerce.date()
+    })
+
+    const { date } = getDayParams.parse(req.query)
+
+    const parsedDate = dayjs(date).startOf('day')
+    const weekDay = parsedDate.get('day')
+
+    // todos hábitos possíveis
+    // hábitos já completados
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true
+      }
+    })
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id
+    })
+
+    return {
+      possibleHabits,
+      completedHabits
+    }
+  })
 }
